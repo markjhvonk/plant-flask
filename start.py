3b796f11bbe0bbfe5214dc1f49be061b0a1e2fd1
Mark Vonk <markjhvonk@gmail.com>
2019-09-30 22:52:50 +0200

    Added mongo functionality

--- start.py
+++ start.py
@@ -1,14 +1,34 @@
 from flask import Flask, request
+from pymongo import MongoClient
+from bson.json_util import dumps
+import datetime
+import json
 
+# mongodb setup
+client = MongoClient()
+plantdb = client.plantdb
+collection = plantdb.plant_collection
+
+# flask setup
 app = Flask(__name__)
 
-
+# root route for plant api
 @app.route("/", methods=["GET", "POST"])
 def users():
     if request.method == 'POST':
-        return 'Put something to post here'
+        data = json.loads(request.data)
+        value = data['value']
+
+        if value:
+            collection.insert({
+                "timestamp": datetime.datetime.now().isoformat(),
+                "value": value,
+            })
+        else:
+            return "please provide value"
+        return str(value), 201
     else:
-        return '{ "name":"John", "age":30, "city":"New York"}'
+        return dumps(collection.find({}))
 
 
 if __name__ == "__main__":
