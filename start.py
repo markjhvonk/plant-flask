0196d0aed7eb7b31ebff7911e3d365fb6da157e3
Mark Vonk <markjhvonk@gmail.com>
2019-10-15 22:19:53 +0200

    Update port and CORS

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from pymongo import MongoClient
 from bson.json_util import dumps
+from flask_cors import CORS
 import datetime
 import json
 
@@ -10,15 +11,16 @@ plantdb = client.plantdb
 collection = plantdb.plant_collection
 
 # flask setup
-application = Flask(__name__)
+app = Flask(__name__)
+CORS(app)
 
 
-@application.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def home():
     return "Welcome to the plant-flask-api"
 
 # root route for plant api
-@application.route("/api", methods=["GET", "POST"])
+@app.route("/api", methods=["GET", "POST"])
 def users():
     if request.method == 'POST':
         data = json.loads(request.data)
@@ -37,4 +39,5 @@ def users():
 
 
 if __name__ == "__main__":
-    application.run(port=90)
+    # app.run()
+    app.run(host='0.0.0.0')
