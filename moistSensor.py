0fe38d3a8569857bb3abb1dd31ccbe72fc59fd99
Mark Vonk <markjhvonk@gmail.com>
2019-10-16 21:24:15 +0200

    Fixed pull_up parameter

--- moistSensor.py
+++ moistSensor.py
@@ -1,7 +1,7 @@
 from gpiozero import InputDevice
 from time import sleep
 
-sensor = InputDevice(17, pull_up=true)
+sensor = InputDevice(17, pull_up=True)
 
 while True:
     print('is_active?')
--- start.py
+++ start.py
@@ -39,5 +39,5 @@ def users():
 
 
 if __name__ == "__main__":
-    # app.run()
-    app.run(host='0.0.0.0')
+    app.run(host='127.0.0.1')
+    # app.run(host='0.0.0.0')
